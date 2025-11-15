6e6cd4f2325bc20ed231556b7ba47030543727f3
Rohit Singh Bisht <rohitsinghbisht568@gmail.com>
2025-11-15 14:26:38 +0530

    edits names some files

---
+++ DSA_Question_Sheets/Q1.C
@@ -0,0 +1,10 @@
+// Print the message along with values and sum (Ex - The sum of 45 & 12 = 57)
+
+#include<stdio.h>
+int main()
+{
+    int a = 45;
+    int b = 12;
+    printf("The Sum of %d & %d = %d", a, b, a+b);
+    return 0; 
+}
---
+++ squareNumber.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a = 2, b = 3;
+    int power = pow(a , b);
+    printf("Number %d^%d Square is = %d",a, b, power);
+    return 0;
+}
