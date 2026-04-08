5de2257d79f954afbedf1df4d0617e9d922c17be
Rohit Singh Bisht <rohitsinghbisht568@gmail.com>
2026-04-08 22:38:22 +0530

    Add program to check grades based on marks input

---
+++ Conditional_Statement/if_Else_If/chackGrades.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int marks;
+    printf("Enter your marks : ");
+    scanf("%d", &marks);
+
+    if(marks >= 90 &&  marks <= 100){
+        printf("Grade A+");
+    }
+    else if(marks >= 80 &&  marks < 90){
+        printf("Grade A");
+    }
+    else if(marks >= 70 &&  marks < 80){
+        printf("Grade B+");
+    }
+    else if(marks >= 60 &&  marks < 70){
+        printf("Grade B");
+    }
+    else if(marks >= 50 &&  marks < 60){
+        printf("Grade C");
+    }
+    else if(marks >= 40 &&  marks < 50){
+        printf("Grade D");
+    }
+    else if(marks < 40){
+        printf("Fail");
+    }
+    else{
+        printf("Invalid input.");
+    }
+    return 0;
+}
