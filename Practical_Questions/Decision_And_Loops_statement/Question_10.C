12e00d004dabdb5b0d129dab4052bad6ce4a6452
Rohit Singh Bisht <rohitsinghbisht568@gmail.com>
2026-04-13 00:41:57 +0530

    Add program to check the validity of a triangle based on side lengths

---
+++ Practical_Questions/Decision_And_Loops_statement/Question_10.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int a , b, c;
+    printf("Enter values of three side of triangle :\n");
+    printf("Enter the first side : ");
+    scanf("%d", &a);
+
+    printf("Enter the second side : ");
+    scanf("%d", &b);~
+
+    printf("Enter the third side : ");
+    scanf("%d", &c);
+
+    // check validity
+    if((a + b > c) && (a + c > b) && (b + c > a)){
+        printf("Triangle is Valid\n");
+    }
+    else
+    {
+        printf("Triangle is Not Valid\n");
+    }
+    return 0;
+}
