e74ab06d27829502dc51efed0517208a906c8f2d
Rohit Singh Bisht <rohitsinghbisht568@gmail.com>
2026-08-26 20:23:43 +0530

    Fix formatting in pointer practice and correct syntax in variable size check program

--- Practical_Questions/Pointers/pointer_practice.C
+++ Practical_Questions/Pointers/pointer_practice.C
@@ -1,21 +1,37 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int age = 22;
+//     int *ptr = &age;
+
+//     // for printing addresses
+//     // printf("%p\n", &age);
+//     // printf("%p\n", ptr);
+//     // printf("%p", &ptr);  //  address of pointer
+
+//     // for printing values
+//     printf("%d\n", age ); // 22
+//     printf("%d\n", *ptr); // 22
+//     printf("%d\n", *(&age)); // 22
+
+
+
+//     // printf("%u", &age); 
+//     return 0;
+// }
+
+
+
 #include <stdio.h>
-int main()
-{
-    int age = 22;
-    int *ptr = &age;
 
-    // for printing addresses
-    // printf("%p\n", &age);
-    // printf("%p\n", ptr);
-    // printf("%p", &ptr);  //  address of pointer
+int main() {
+    int age = 25;
 
-    // for printing values
-    printf("%d\n", age ); // 22
-    printf("%d\n", *ptr); // 22
-    printf("%d\n", *(&age)); // 22
+    // Printing the value of the variable
+    printf("Value: %d\n", age);
 
+    // Printing the memory address of the variable
+    printf("Memory Address: %p\n", (void*)&age);
 
-
-    // printf("%u", &age); 
     return 0;
 }
--- checkVariableSize.c
+++ checkVariableSize.c
@@ -5,7 +5,6 @@ int main()
 {
     printf("Size of int: %zu bytes\n", sizeof(int));    
     printf("Size of char: %zu bytes\n", sizeof(char));    
-    printf("Size of float: %zu bytes\n", sizeof(float))   
-   
+    printf("Size of float: %zu bytes\n", sizeof(float));
 }   
 
